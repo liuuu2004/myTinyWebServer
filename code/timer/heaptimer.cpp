4b23ad46e37169eb462eab0824bf7c3fc3ed9640
liuuu2004 <liugongmingg@163.com>
2024-05-22 15:08:49 +0800

    a timer based on heap impl..

--- code/timer/heaptimer.cpp
+++ code/timer/heaptimer.cpp
@@ -1,2 +1,34 @@
 #include "heaptimer.h"
+#include <cassert>
 
+void HeapTimer::shift_up_(size_t i) {
+    assert(i >= 0 && i < heap_.size());
+    size_t j = (i - 1) / 2;
+    while (j >= 0) {
+        if (heap_[i] < heap_[j]) {
+            break;
+        }
+        swap(i, j);
+        i = j;
+        j = (i - 1) / 2;
+    }
+}
+
+bool HeapTimer::shift_down_(size_t i, size_t n) {
+    assert(i >= 0 && i < heap_.size());
+    assert(n >= 0 && n <= heap_.size());
+    size_t j = i;
+    size_t k = j * 2 + 1;
+    while (k < n) {
+        if (k + 1 < n && heap_[k + 1] < heap_[k]) {
+            k++;
+        }
+        if (heap_[j] < heap_[k]) {
+            break;
+        }
+        swap(j, k);
+        j = k;
+        k = j * 2 + 1;
+    }
+    return j > i;
+}
--- code/timer/heaptimer.h
+++ code/timer/heaptimer.h
@@ -46,12 +46,41 @@ public:
     int GetNextTick();
 
 private:
+    /**
+     * remove the TimerNode at the specified index from the heap by swapping it with the
+     * last element and adjust the heap
+     * @param i index of the TimerNode to be deleted
+    */
     void del_(size_t i);
+
+    /**
+     * move the TimerNode at index i up to the heap to maintain the min-heap
+     * @param i index of the TimerNode to be moved upper
+    */
     void shift_up_(size_t i);
+
+    /** 
+     * move the TimerNode at index i down to maintain the min-heap
+     * @param i the index of the TimerNode to be moved down
+     * @param n TODO
+    */
     bool shift_down_(size_t i, size_t n);
+
+    /**
+     * swap the position of the two TimerNodes by their index
+     * @param i node1 index
+     * @param j node2 index
+    */
     void swap(size_t i, size_t j);
 
+    /**
+     * an containner to store all the TimerNodes
+    */
     std::vector<TimerNode> heap_;
+
+    /**
+     * map an id to its position in the map
+    */
     std::unordered_map<int, size_t> ref_;
 };
 
