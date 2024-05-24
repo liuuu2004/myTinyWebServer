5fb17035f068744a5a16b116a0c73b58f8ebeb0a
liuuu2004 <liugongmingg@163.com>
2024-05-24 20:05:23 +0800

    http response impl ..

---
+++ code/http/httpresponse.cpp
@@ -0,0 +1,65 @@
+#include "httpresponse.h"
+#include <cassert>
+#include <string>
+#include <unordered_map>
+
+const std::unordered_map<std::string, std::string> HttpResponse::SUFFIX_TYPE_ = {
+    { ".html",  "text/html" },
+    { ".xml",   "text/xml" },
+    { ".xhtml", "application/xhtml+xml" },
+    { ".txt",   "text/plain" },
+    { ".rtf",   "application/rtf" },
+    { ".pdf",   "application/pdf" },
+    { ".word",  "application/nsword" },
+    { ".png",   "image/png" },
+    { ".gif",   "image/gif" },
+    { ".jpg",   "image/jpeg" },
+    { ".jpeg",  "image/jpeg" },
+    { ".au",    "audio/basic" },
+    { ".mpeg",  "video/mpeg" },
+    { ".mpg",   "video/mpeg" },
+    { ".avi",   "video/x-msvideo" },
+    { ".gz",    "application/x-gzip" },
+    { ".tar",   "application/x-tar" },
+    { ".css",   "text/css "},
+    { ".js",    "text/javascript "},
+};
+
+const std::unordered_map<int, std::string> HttpResponse::CODE_STATUS_ = {
+    { 200, "OK" },
+    { 400, "Bad Request" },
+    { 403, "Forbidden" },
+    { 404, "Not Found" },
+};
+
+const std::unordered_map<int, std::string> HttpResponse::CODE_PATH_ = {
+    { 400, "/400.html" },
+    { 403, "/403.html" },
+    { 404, "/404.html" },
+};
+
+HttpResponse::HttpResponse() {
+    code_ = -1;
+    is_keep_alive_ = false;
+    path_ = "";
+    src_dir_ = "";
+    mm_file_ = nullptr;
+    mm_file_stat_ = {0};
+}
+
+HttpResponse::~HttpResponse() {
+    unmap_file();
+}
+
+void HttpResponse::init(const std::string &src_dir, std::string &path, bool is_keep_alive, int code) {
+    assert(src_dir != "");
+    if (mm_file_ != nullptr) {
+        unmap_file();
+    }
+    code_= code;
+    path_ = path;
+    is_keep_alive_ = is_keep_alive;
+    src_dir_ = src_dir;
+    mm_file_ = nullptr;
+    mm_file_stat_ = {0};
+}
--- code/http/httpresponse.h
+++ code/http/httpresponse.h
@@ -12,6 +12,13 @@ class HttpResponse {
 public:
     HttpResponse();
     ~HttpResponse();
+    /**
+     * init variables with specific value
+     * @param src_dir diractor to store resources
+     * @param path path to store resource
+     * @param is_keep_alive shall we keep the connction alive for further requests
+     * @param code HTTP reaponse code
+    */
     void init(const std::string &src_dir, std::string &path, bool is_keep_alive = false, int code = -1);
     void make_response(Buffer &buffer);
     void unmap_file();
@@ -26,18 +33,52 @@ private:  // methods
     void add_content(Buffer &buffer);
     void error_html();
     std::string get_file_type();
-    
+
 private:  // variables
+    /**
+     * store HTTP response code
+    */
     int code_;
+
+    /**
+     * indicates whether the connection should be kept alive. if true, the connection is
+     * kept for further requests
+    */
     bool is_keep_alive_;
+
+    /**
+     * store the path to the requested resource or file
+    */
     std::string path_;
+
+    /**
+     * stores the source dictionary path where the requested reesources or files are located
+    */
     std::string src_dir_;
 
+    /**
+     * pointer to the memory-mapped file. used to access file content efficiently
+    */
     char *mm_file_;
+
+    /**
+     * stores the metadatta(e.g. size, permissions) of the memory-mapped file
+    */
     struct stat mm_file_stat_;
 
+    /**
+     * maps file exxtensions to their corresponding MIME types(e.g. html -> test/heml)
+    */
     static const std::unordered_map<std::string, std::string> SUFFIX_TYPE_;
+
+    /**
+     * maps HTTP status codes to their corresponding status maeesges(e.g. 200 -> OK)
+    */
     static const std::unordered_map<int, std::string> CODE_STATUS_;
+
+    /**
+     * maps error status codes to their corresponding error HTML paths(e.g. 404 -> /40.html)
+    */
     static const std::unordered_map<int, std::string> CODE_PATH_;
 };
 
