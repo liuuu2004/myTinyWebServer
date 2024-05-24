8de252e81301716a78f50f0d1f397d0b425c5567
liuuu2004 <liugongmingg@163.com>
2024-05-24 16:46:58 +0800

    sql connection RAII

---
+++ code/pool/sqlconnRAII.h
@@ -0,0 +1,27 @@
+#ifndef SQLCONNRAII_H_
+#define SQLCONNRAII_H_
+
+#include "sqlconnpool.h"
+#include <cassert>
+
+class SqlConnRAII {
+public:
+    SqlConnRAII(MYSQL **sql, SqlConnPool *conn_pool) {
+        assert(conn_pool != nullptr);
+        *sql = conn_pool->get_conn();
+        sql_ = *sql;
+        conn_pool_ = conn_pool;
+    }
+
+    ~SqlConnRAII() {
+        if (sql_ != nullptr) {
+            conn_pool_->free_conn(sql_);
+        }
+    }
+private:
+    MYSQL *sql_;
+    SqlConnPool *conn_pool_;
+};
+
+
+#endif
