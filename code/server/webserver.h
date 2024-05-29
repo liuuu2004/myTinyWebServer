126da0a9330cf812d1141c25a1ffdc5b383188ce
liuuu2004 <liugongmingg@163.com>
2024-05-29 17:27:03 +0800

    bone of web-server

--- code/server/webserver.h
+++ code/server/webserver.h
@@ -3,6 +3,7 @@
 
 #include <cstdint>
 #include <memory>
+#include <netinet/in.h>
 #include <unordered_map>
 
 #include "../timer/heaptimer.h"
@@ -12,8 +13,31 @@
 
 class WebServer {
 public:
-
+    WebServer(int port, int trig_mode, int timeout_ms, bool opt_linger, int sql_port,
+              const char *sql_user, const char *sql_pwd, const char *db_name,
+              int conn_pool_num, int thread_num, bool open_log, int log_level, int log_que_size);
+    ~WebServer();
+    void start();
+    
 private:
+    bool init_socket();
+    void init_event_mode(int trig_mode);
+    void add_client(int fd, sockaddr_in addr);
+
+    void deal_listen();
+    void deal_write(HttpConn *client);
+    void deal_read(HttpConn *client);
+
+    void send_error(int fd, const char *info);
+    void extent_time(HttpConn *client);
+    void close_conn(HttpConn *client);
+
+    void on_read(HttpConn *client);
+    void on_write(HttpConn *client);
+    void on_process(HttpConn *client);
+
+    static const int MAX_FD_ = 65535;
+    static int set_fd_nonblock(int fd);
 
 private:
     int port_;
